4b1173f58bd59e50f071f8d54f93f5c62d2d1540
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-06-07 18:19:11 +0100

    port set

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const htmlRoutes = require("./routes/htmlroutes");
+const apiRoutes = require("./routes/api");
+const uuid = require("uuid");
+
+const app = express();
+var PORT = process.env.PORT || 4023;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+//Start listen
+app.listen(PORT, function () {
+  console.log(`App listening on PORT http://localhost:${PORT} `);
+});
